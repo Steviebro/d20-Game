b6e1fe5b7ce3fc9b5995049e90f2e1fd57838847
aandrewoh <andrew.oh02@gmail.com>
2024-04-03 19:02:43 -0400

    added #include <vector> to Item.h

--- cmake-build-debug/CMakeFiles/345Project.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/345Project.dir/DependInfo.cmake
@@ -8,15 +8,15 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/Users/andrewoh/CLionProjects/345Project/src/Components/Armor.cpp" "CMakeFiles/345Project.dir/src/Components/Armor.cpp.o" "gcc" "CMakeFiles/345Project.dir/src/Components/Armor.cpp.o.d"
-  "/Users/andrewoh/CLionProjects/345Project/src/Components/Character.cpp" "CMakeFiles/345Project.dir/src/Components/Character.cpp.o" "gcc" "CMakeFiles/345Project.dir/src/Components/Character.cpp.o.d"
-  "/Users/andrewoh/CLionProjects/345Project/src/Components/Equipment.cpp" "CMakeFiles/345Project.dir/src/Components/Equipment.cpp.o" "gcc" "CMakeFiles/345Project.dir/src/Components/Equipment.cpp.o.d"
-  "/Users/andrewoh/CLionProjects/345Project/src/Components/Item.cpp" "CMakeFiles/345Project.dir/src/Components/Item.cpp.o" "gcc" "CMakeFiles/345Project.dir/src/Components/Item.cpp.o.d"
-  "/Users/andrewoh/CLionProjects/345Project/src/Components/ItemBag.cpp" "CMakeFiles/345Project.dir/src/Components/ItemBag.cpp.o" "gcc" "CMakeFiles/345Project.dir/src/Components/ItemBag.cpp.o.d"
-  "/Users/andrewoh/CLionProjects/345Project/src/Components/Weapon.cpp" "CMakeFiles/345Project.dir/src/Components/Weapon.cpp.o" "gcc" "CMakeFiles/345Project.dir/src/Components/Weapon.cpp.o.d"
-  "/Users/andrewoh/CLionProjects/345Project/src/Static/Dice.cpp" "CMakeFiles/345Project.dir/src/Static/Dice.cpp.o" "gcc" "CMakeFiles/345Project.dir/src/Static/Dice.cpp.o.d"
-  "/Users/andrewoh/CLionProjects/345Project/src/Static/Functions.cpp" "CMakeFiles/345Project.dir/src/Static/Functions.cpp.o" "gcc" "CMakeFiles/345Project.dir/src/Static/Functions.cpp.o.d"
-  "/Users/andrewoh/CLionProjects/345Project/src/main.cpp" "CMakeFiles/345Project.dir/src/main.cpp.o" "gcc" "CMakeFiles/345Project.dir/src/main.cpp.o.d"
+  "/Users/andrewoh/CLionProjects/345Projecta/src/Components/Armor.cpp" "CMakeFiles/345Project.dir/src/Components/Armor.cpp.o" "gcc" "CMakeFiles/345Project.dir/src/Components/Armor.cpp.o.d"
+  "/Users/andrewoh/CLionProjects/345Projecta/src/Components/Character.cpp" "CMakeFiles/345Project.dir/src/Components/Character.cpp.o" "gcc" "CMakeFiles/345Project.dir/src/Components/Character.cpp.o.d"
+  "/Users/andrewoh/CLionProjects/345Projecta/src/Components/Equipment.cpp" "CMakeFiles/345Project.dir/src/Components/Equipment.cpp.o" "gcc" "CMakeFiles/345Project.dir/src/Components/Equipment.cpp.o.d"
+  "/Users/andrewoh/CLionProjects/345Projecta/src/Components/Item.cpp" "CMakeFiles/345Project.dir/src/Components/Item.cpp.o" "gcc" "CMakeFiles/345Project.dir/src/Components/Item.cpp.o.d"
+  "/Users/andrewoh/CLionProjects/345Projecta/src/Components/ItemBag.cpp" "CMakeFiles/345Project.dir/src/Components/ItemBag.cpp.o" "gcc" "CMakeFiles/345Project.dir/src/Components/ItemBag.cpp.o.d"
+  "/Users/andrewoh/CLionProjects/345Projecta/src/Components/Weapon.cpp" "CMakeFiles/345Project.dir/src/Components/Weapon.cpp.o" "gcc" "CMakeFiles/345Project.dir/src/Components/Weapon.cpp.o.d"
+  "/Users/andrewoh/CLionProjects/345Projecta/src/Static/Dice.cpp" "CMakeFiles/345Project.dir/src/Static/Dice.cpp.o" "gcc" "CMakeFiles/345Project.dir/src/Static/Dice.cpp.o.d"
+  "/Users/andrewoh/CLionProjects/345Projecta/src/Static/Functions.cpp" "CMakeFiles/345Project.dir/src/Static/Functions.cpp.o" "gcc" "CMakeFiles/345Project.dir/src/Static/Functions.cpp.o.d"
+  "/Users/andrewoh/CLionProjects/345Projecta/src/main.cpp" "CMakeFiles/345Project.dir/src/main.cpp.o" "gcc" "CMakeFiles/345Project.dir/src/main.cpp.o.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- cmake-build-debug/CMakeFiles/Makefile.cmake
+++ cmake-build-debug/CMakeFiles/Makefile.cmake
@@ -105,7 +105,7 @@ set(CMAKE_MAKEFILE_DEPENDS
   "/Applications/CLion.app/Contents/bin/cmake/mac/aarch64/share/cmake-3.27/Modules/Platform/Darwin.cmake"
   "/Applications/CLion.app/Contents/bin/cmake/mac/aarch64/share/cmake-3.27/Modules/Platform/UnixPaths.cmake"
   "/Applications/CLion.app/Contents/bin/cmake/mac/aarch64/share/cmake-3.27/Modules/ProcessorCount.cmake"
-  "/Users/andrewoh/CLionProjects/345Project/CMakeLists.txt"
+  "/Users/andrewoh/CLionProjects/345Projecta/CMakeLists.txt"
   "CMakeFiles/3.27.8/CMakeCCompiler.cmake"
   "CMakeFiles/3.27.8/CMakeCXXCompiler.cmake"
   "CMakeFiles/3.27.8/CMakeSystem.cmake"
